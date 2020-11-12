c38e7e896f311297590cd6b8f0961887dd839f55
NSerus <nunoworkingserus@gmail.com>
2020-11-12 09:42:23 +0000

    Finnished book model

--- Books/Models/Book.cs
+++ Books/Models/Book.cs
@@ -13,5 +13,11 @@ namespace Books.Models
         [Required]
         [StringLength(512)]
         public string Title { get; set; }
+
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+
     }
 }
