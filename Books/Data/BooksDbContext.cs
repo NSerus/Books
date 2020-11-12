66ccd96f812556be34dd4dd9a78c5b0c761ccd30
NSerus <nunoworkingserus@gmail.com>
2020-11-12 10:31:20 +0000

    Authors Scaffolding

---
+++ Books/Data/BooksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Books.Models;
+
+namespace Books.Data
+{
+    public class BooksDbContext : DbContext
+    {
+        public BooksDbContext (DbContextOptions<BooksDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Books.Models.Author> Author { get; set; }
+    }
+}
