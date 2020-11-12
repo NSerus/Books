489390411a26131f863d0cca69334d1b71c77d11
NSerus <nunoworkingserus@gmail.com>
2020-11-12 09:39:15 +0000

    added id and title

--- Books/Models/Book.cs
+++ Books/Models/Book.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Books.Models
 {
     public class Book
     {
-
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(512)]
+        public string Title { get; set; }
     }
 }
